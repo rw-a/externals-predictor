d9f903dabc452c8eedf6b7ab6c44b42c8afc2901
rw-a <rwa2684@gmail.com>
2023-05-28 09:32:10 +1000

    updated description

--- src/App.tsx
+++ src/App.tsx
@@ -80,7 +80,7 @@ export default function Calculator() {
 					Enter your internal results and this will attempt to predict what score 
 					you will get in the externals by mapping your percentile.
 					QCAA neither endorses nor is affiliated with this website. 
-					Nobody can perfectly predict the future perfectly, so use at your own risk! 
+					Predicting the future perfectly is impossible, so use at your own risk! 
 				</p>
 				<YearSelector onYearSelect={handleYearSelect}></YearSelector>
 			</div>	
